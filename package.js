52dfc8fbd5a5cb5fe9e2cf23879d49ed029cb91d
aaronjudd <aaronjudd@me.com>
2015-02-03 10:56:51 -0800

    updated version

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   summary: "Reaction Google Analytics - GA for Reaction Commece",
   name: "reactioncommerce:reaction-google-analytics",
-  version: "0.1.4",
+  version: "0.2.1",
   git: "https://github.com/ongoworks/reaction-google-analytics.git"
 });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.0');
   api.use("meteor-platform@1.2.1");
   api.use("coffeescript");
   api.use("less");
-  api.use("reactioncommerce:core@0.2.3");
+  api.use("reactioncommerce:core@0.3.0");
 
   api.add_files([
     "common/register.coffee",
