a5e0cd370c9c054d2e24c25c68e7c9f489e77583
aaronjudd <aaronjudd@me.com>
2015-02-13 19:06:24 -0800

    Updated for package-registry-refactor
    Issue reactioncommerce/reaction#314

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Reaction Google Analytics - GA for Reaction Commece",
   name: "reactioncommerce:reaction-google-analytics",
-  version: "0.2.1",
+  version: "0.3.0",
   git: "https://github.com/ongoworks/reaction-google-analytics.git"
 });
 
@@ -10,22 +10,24 @@ Package.on_use(function (api, where) {
   api.use("meteor-platform@1.2.1");
   api.use("coffeescript");
   api.use("less");
-  api.use("reactioncommerce:core@0.3.0");
+  api.use("reactioncommerce:core@0.4.0");
 
   api.add_files([
-    "common/register.coffee",
+    "common/routing.coffee",
     "common/collections.coffee"
   ], ["client", "server"]);
+
   api.add_files([
     "client/compatibility/google-analytics-bootstrap.js",
-    "client/routing.coffee",
     "client/startup.coffee",
     "client/templates/googleAnalytics/googleAnalytics.html",
     "client/templates/googleAnalytics/googleAnalytics.coffee"
   ], ["client"]);
+
   api.add_files([
     "server/security/AnalyticsEvents.coffee",
     "server/publications.coffee",
-    "server/fixtures.coffee"
+    "server/fixtures.coffee",
+    "server/register.coffee"
   ], ["server"]);
 });
